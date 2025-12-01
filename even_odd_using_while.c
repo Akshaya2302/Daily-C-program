529fac01a69c84d3ae0e9d476b0b19ef4f7a1595
Akshaya C S <akshayacs2008@gmail.com>
2025-12-01 21:12:21 +0530

    Add program to print even and odd numbers
    using while loop i have printed till 10 i mean lesser than 10 even and odd numbers

---
+++ even_odd_using_while.c
@@ -0,0 +1,24 @@
+//odd
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while(i<=10)
+	{
+		printf("%d\n",i);
+		i+=2;
+	}
+	return 0;
+}
+//even
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	while(i<=10)
+	{
+		printf("%d\n",i);
+		i+=2;
+	}
+	return 0;
+}
