07e665fcf97c924e47ec6f7dbc692478bd192894
Akshaya C S <akshayacs2008@gmail.com>
2025-12-26 20:46:12 +0530

    Implement even-odd checker using an array
    The program reads ten integers from the user and prints which of them are odd.

---
+++ check_even_odd_using_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a[10];
+	int i;
+	printf("enter the ten number: ");
+	for(i=0;i<10;i++)
+	{
+	    scanf("%d",&a[i]);	
+	} 
+	for(i=0;i<10;i++)
+	{
+		if(a[i]%2 != 0)
+		{
+			printf("%d is odd number\n",a[i]);
+		}
+	}
+	return 0;
+}
